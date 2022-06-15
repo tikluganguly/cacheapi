c8fb866dd24a108cacfce456bcbe0e7b5741dee9
tiklu <tikluganguly@hotmail.com>
2022-06-16 01:59:58 +0530

    little code cleanup to add proper serves

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
+//use warp::Reply;
 use warp::Filter;
+
+/*fn strVal(name: String)->Reply {
+    println!("Got name : {}", name);
+    format!("Hello, {}!", name)
+}*/
 
 #[tokio::main]
 async fn main() {
     // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let hello = warp::path!("str" / String).map(|name| format!("Hello, {}!", name));
+    let hello = warp::path!("str" / String).map(|name| {
+        println!("Got name : {}", name);
+        format!("Hello, {}!", name)
+    });
 
-    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+    let port = 3030;
+    println!("Serving the memory db at : {}", port);
+    warp::serve(hello).run(([127, 0, 0, 1], port)).await;
 }
